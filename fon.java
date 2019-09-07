d7a9682b53d02f0ce3f144723f490bef5af9e97d
kanokwanfpp <42598549+kanokwanfpp@users.noreply.github.com>
2019-09-07 21:53:23 +0700

    Add files via upload

---
+++ fon.java
@@ -0,0 +1,21 @@
+public class fon {
+ public static void main(String a[]) {
+boolean ff=false;
+byte oo=9;
+short nn=-5;
+int aa=10;
+long bb=22;
+float cc=25.333f;
+double dd=100;
+char ee='K';
+
+System.out.println(ff);
+System.out.println(oo);
+System.out.println(nn);
+System.out.println(aa); 
+System.out.println(bb);
+System.out.println(cc);
+System.out.println(dd);
+System.out.println(ee);
+}
+}
